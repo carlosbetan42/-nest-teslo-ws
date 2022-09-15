51e4daaa513fe20f98430672b55122a29235a16e
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2022-09-15 16:17:33 -0500

    Change url to production

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,7 +3,7 @@ import { Manager, Socket } from 'socket.io-client';
 let socket: Socket;
 
 export const connectToServer = (token: string) => {
-  const manager = new Manager('http://localhost:3001/socket.io/socket.io.js', {
+  const manager = new Manager('https://teslo-nest-b.herokuapp.com/socket.io/socket.io.js', {
     extraHeaders: {
       hola: 'mundo',
       authentication: token,
